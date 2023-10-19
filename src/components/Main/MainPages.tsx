54fb4a63a414567750abb00199f0dcfe4e060d85
m-diana-dev <m.diana.dev@gmail.com>
2023-10-19 15:41:10 +0300

    fixed path

--- src/components/Main/MainPages.tsx
+++ src/components/Main/MainPages.tsx
@@ -22,7 +22,7 @@ type MainPagesPropsType = {
 export const MainPages = (props: MainPagesPropsType) => {
     return (
         <Theme>
-            <BrowserRouter>
+            <BrowserRouter basename="/connect">
                 <div>
                     <HeaderContainer/>
                     <Section>
