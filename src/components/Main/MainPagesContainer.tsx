8903a49920bb0b42acbdaf264c4fa67a27fabc7f
m-diana-dev <m.diana.dev@gmail.com>
2023-12-11 17:05:58 +0300

    added new functionality for maping friends (will be finalized)

--- src/components/Main/MainPagesContainer.tsx
+++ src/components/Main/MainPagesContainer.tsx
@@ -5,10 +5,10 @@ import {followUsersTC, toggleIsLoading, unfollowUsersTC, UserType} from "../../r
 import {connectAPI} from "../../api/api.ts";
 import React from "react";
 import {MainPages} from "./MainPages.tsx";
-import {initializedAppTC} from "./../../redux/app-reducer.ts";
+import {initializedAppTC, SetInitialized} from "./../../redux/app-reducer.ts";
 import {selectFriends} from "../../redux/selectors/friends-selectors.ts";
 import {selectInitialized} from "../../redux/selectors/app-selectors.ts";
-import {selectIsFollowing} from "../../redux/selectors/users-selectors.ts";
+import {selectIsFollowing, totalUsersCount} from "../../redux/selectors/users-selectors.ts";
 import {Preloader} from "../Preloader/Preloader.tsx";
 
 
@@ -16,6 +16,7 @@ type mapStateToPropsType = {
     friends: UserType[]
     isFollowing: number[]
     initialized: boolean
+    totalUsers: number
 }
 type mapDispatchToPropsType = {
     SetFriends: (friends: UserType[]) => void
@@ -23,19 +24,46 @@ type mapDispatchToPropsType = {
     unfollowUsersTC: (userID: number) => void
     followUsersTC: (userID: number) => void
     initializedAppTC: () => void
+    SetInitialized:(initialized: boolean)=>void
 }
 type MainPagesContainerPropsType = mapStateToPropsType & mapDispatchToPropsType
 
 class MainPagesContainer extends React.Component<MainPagesContainerPropsType> {
+    // componentDidMount() {
+    //     this.props.toggleIsLoading(true)
+    //     connectAPI.getUsers(100, 1)
+    //         .then((response) => {
+    //             const friends: UserType[] = response.items.filter((el: UserType) => el.followed)
+    //             this.props.SetFriends(friends)
+    //             this.props.toggleIsLoading(false)
+    //         })
+    //     this.props.initializedAppTC()
+    // }
     componentDidMount() {
-        this.props.toggleIsLoading(true)
-        connectAPI.getUsers(100, 1)
-            .then((response) => {
-                const friends: UserType[] = response.items.filter((el: UserType) => el.followed)
-                this.props.SetFriends(friends)
-                this.props.toggleIsLoading(false)
-            })
         this.props.initializedAppTC()
+        this.props.toggleIsLoading(true);
+        this.props.SetInitialized(false)
+
+        const pageCount = 10;
+        const requests = Array.from({ length: pageCount }, (_, index) =>
+            connectAPI.getUsers(100, index + 1)
+        );
+
+        Promise.all(requests)
+            .then((responses) => {
+                const allFriends = responses
+                    .flatMap((response) => response.items)
+                    .filter((el: UserType) => el.followed);
+
+
+                this.props.SetFriends(allFriends);
+                this.props.toggleIsLoading(false);
+                this.props.SetInitialized(true)
+            })
+            .catch((error) => {
+                console.error("Error fetching friends:", error);
+                this.props.toggleIsLoading(false);
+            });
     }
 
     render() {
@@ -50,7 +78,8 @@ const mapStateToProps = (state: AppStateType): mapStateToPropsType => {
     return {
         friends: selectFriends(state),
         isFollowing: selectIsFollowing(state),
-        initialized: selectInitialized(state)
+        initialized: selectInitialized(state),
+        totalUsers: totalUsersCount(state)
     }
 }
 export default connect(mapStateToProps, {
@@ -58,5 +87,6 @@ export default connect(mapStateToProps, {
     toggleIsLoading,
     unfollowUsersTC,
     followUsersTC,
-    initializedAppTC
+    initializedAppTC,
+    SetInitialized
 })(MainPagesContainer)
