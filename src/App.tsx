a5af8611f9e7cda069ccb86718c5411c7be3046e
m-diana-dev <m.diana.dev@gmail.com>
2024-04-15 15:48:24 +0300

    lesson 95 (HashRouter)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import {AppStateType} from "./redux/redux-store.ts";
 import MainPagesContainer from "./components/Main/MainPagesContainer.tsx";
 import {Theme} from "./styles/Theme.tsx";
-import {BrowserRouter} from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import {Preloader} from "./components/common/Preloader/Preloader.tsx";
 import {Suspense} from "react";
 
@@ -12,11 +12,11 @@ export const App = ({state}: AppPropsType) => {
     console.log(state)
     return (
         <Theme>
-            <BrowserRouter basename="/connect">
+            <HashRouter>
                 <Suspense fallback={<div><Preloader /></div>}>
                     <MainPagesContainer/>
                 </Suspense>
-            </BrowserRouter>
+            </HashRouter>
         </Theme>
     );
 };
